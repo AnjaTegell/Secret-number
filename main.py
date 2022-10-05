6efba8453ca3dbf74fda80f7474e0ea5e5c41387
AnjaTegel <112825545+AnjaTegel@users.noreply.github.com>
2022-10-05 22:07:43 +0200

    Vaja dodatek

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-secret = 19
-guess = int(input("What is our secret number?: "))
+import random
 
-if guess == secret:
-    print ("Congratualations, we have a winner! The secret number is " + str(secret) +"!")
-else:
-    print ("Try again, the number we are looking for is not " + str(guess) + "!")
+secret = random.randint (1,30)
+print ("Pssst, secret is: " + str(secret))
+
+while True:
+    guess = int(input("What is our secret number?: "))
+    if guess == secret:
+        print("Congratualations, we have a winner! The secret number is " + str(secret) + "!")
+        break
+    elif guess > secret:
+        print("Try a smaller number")
+    elif guess < secret:
+        print("Try a bigger number")
+    else:
+        print("Try again, the number we are looking for is not " + str(guess) + "!")
